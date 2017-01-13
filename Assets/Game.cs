655708bb0afbe1a9db3fd50bcc55ef92db30ca2a
Andrés Vargas <tcarlosvargas@gmail.com>
2017-01-12 21:45:12 -0500

    init commit
    Init Commit

---
+++ Assets/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	private HUD mainHud;
+
+    private void Awake(){
+        mainHud = new HUD();
+    }
+
+    void Start () {
+        mainHud.manageHUD(HUD.stateHUD.Splash);
+    }
+	
+	void Update () {
+        StartCoroutine(mainHud.fadeView(HUD.stateHUD.Main, 5f));
+	}
+
+    public void changeScreen(HUD.stateHUD state) {
+    }
+}
---
+++ Assets/HUD.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD{
+
+    private GameObject mainHUD;
+    private Button[] buttons;
+    public enum stateHUD {Splash, Main, Settings, Winner, Loser};
+
+    public GameObject objectHUD{
+        get {
+            return this.mainHUD;
+        }
+    }
+
+    public HUD(){
+        this.mainHUD = GameObject.Find("HUD");
+        this.buttons = this.mainHUD.GetComponents<Button>();
+
+        Debug.Log(buttons.Length);
+    }
+
+    public void manageHUD(stateHUD state) {
+        this.hideHud();
+        switch (state) {
+            case stateHUD.Splash:
+                this.mainHUD.transform.GetChild(0).gameObject.SetActive(true);
+                break;
+            case stateHUD.Main:
+                this.mainHUD.transform.GetChild(1).gameObject.SetActive(true);
+                break;
+            case stateHUD.Settings:
+                this.mainHUD.transform.GetChild(2).gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void hideHud() {
+        for(int i = 0; i < this.mainHUD.transform.childCount; i++) {
+            this.mainHUD.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public void resetHud() {
+        this.hideHud();
+        this.manageHUD(stateHUD.Splash);
+    }
+
+    public IEnumerator fadeView(stateHUD state, float time) {
+        yield return new WaitForSeconds(time);
+        this.manageHUD(state);
+    }
+
+
+
+
+
+
+
+}
